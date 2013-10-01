dbe14b93a1f5107964c1301461a6510bace58341
Archie <cranktrooper@gmail.com>
2013-10-01 22:55:59 +0800

    Upload all files

--- js/jquery.yasp.js
+++ js/jquery.yasp.js
@@ -1,1 +1,17 @@
+(function($){
+    $.fn.yasp = function(config) {
+        var scrollbox = $('<div id="jq-yasp-scrollbox"></div>');
+        var scrollbar = $('<div id="jq-yasp-scrollbar"></div>');
+        scrollbox.append(scrollbar);
 
+        var clip = $('<div class="jq-yasp-clip"></div>');
+
+        var target = $(this);
+
+        target.wrap(clip);
+
+        // append scrollbox to the target
+        target.append(scrollbox);
+
+    };
+}(jQuery));
